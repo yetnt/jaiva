884a622f3b1ff63307256bb7992325e4442814d9
Lehlogonolo Poole <hlonipoole692@gmail.com>
2025-01-24 19:29:43 +0200

    main change

--- src/main/java/com/yetnt/Main.java
+++ src/main/java/com/yetnt/Main.java
@@ -1,7 +1,22 @@
 package com.yetnt;
 
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File myObj = new File("C://Users//ACER//Documents//code//jaiva//src//main//resources//test.txt");
+        try {
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
