faeea8bcb276de7aa26dd6e52ac5e2f00fd95dd7
Lehlogonolo Poole <hlonipoole692@gmail.com>
2025-01-26 17:02:24 +0200

    Main (Still not ready)

--- src/main/java/com/yetnt/Main.java
+++ src/main/java/com/yetnt/Main.java
@@ -2,20 +2,39 @@ package com.yetnt;
 
 import java.io.File; // Import the File class
 import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.ArrayList;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Main {
     public static void main(String[] args) {
-        File myObj = new File("C://Users//ACER//Documents//code//jaiva//src//main//resources//test.txt");
+        File myObj = new File("C://Users//ACER//Documents//code//jaiva//src//main//resources//test2.jiv");
         try {
+            ArrayList<Token<?>> tokens = new ArrayList<>();
+            Tokenizer.MultipleLinesOutput m = null;
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+                String previousLine = "";
+                String line = myReader.nextLine();
+                Object something = Tokenizer.readLine(line, previousLine, m);
+                if (something instanceof Tokenizer.MultipleLinesOutput) {
+                    m = (Tokenizer.MultipleLinesOutput) something;
+                } else if (something instanceof ArrayList<?>) {
+                    m = null;
+                    tokens.addAll((ArrayList<Token<?>>) something);
+
+                }
             }
+
+            for (Token<?> t : tokens) {
+                System.err.println(t.toString());
+            }
+            System.out.println(tokens.size());
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("errrorrrr");
             e.printStackTrace();
         }
     }
