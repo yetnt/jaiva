9f3fd7b9cd795798ef1b338d8fd391a690b4f94a
Lehlogonolo Poole <hlonipoole692@gmail.com>
2025-01-26 17:02:05 +0200

    more testing code

--- src/test/java/playground.java
+++ src/test/java/playground.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import com.yetnt.*;
 import com.yetnt.Token.TBooleanVar;
 import com.yetnt.Token.TIntVar;
-import com.yetnt.Token.TStringVar;
+import com.yetnt.Token.TStringVar;;
 
 public class playground {
     public static int findEnclosingCharIndex(String line, char start, char end) {
@@ -33,14 +33,17 @@ public class playground {
     }
 
     public static void main(String[] args) {
-        String stuff = "maak lol\n<- aowa!\nmaak poop <- 100!";
-        int lineNum = 0;
+        String stuff = "maak lol<- yebo!maak poop <- 100!maak lk <- \"RNADOM STRING IWTH A \n\"!";
         try {
             @SuppressWarnings("unchecked")
-            ArrayList<Token<?>> tokens = (ArrayList<Token<?>>) Tokenizer.readLine(stuff, "");
-            @SuppressWarnings({ "unchecked", "rawtypes" })
-            Token<TBooleanVar>.TBooleanVar token = (Token<TBooleanVar>.TBooleanVar) tokens.get(0).getValue();
-            System.out.println(token.value);
+            ArrayList<Token<?>> tokens = (ArrayList<Token<?>>) Tokenizer.readLine(stuff, "", null);
+            // @SuppressWarnings({ "unchecked", "rawtypes" })
+            Token<TBooleanVar>.TBooleanVar lol = (Token<TBooleanVar>.TBooleanVar) tokens.get(0).getValue();
+            Token<TIntVar>.TIntVar poop = (Token<TIntVar>.TIntVar) tokens.get(1).getValue();
+            Token<TStringVar>.TStringVar lk = (Token<TStringVar>.TStringVar) tokens.get(2).getValue();
+            System.out.println(lol.value);
+            System.out.println(poop.value);
+            System.out.println(lk.value);
         } catch (Exception e) {
             e.printStackTrace();
         }
