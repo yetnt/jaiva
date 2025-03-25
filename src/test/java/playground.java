f2ca853f7c9692fa9a7accb4d20a255e7fc3e89c
Lehlogonolo Poole <hlonipoole692@gmail.com>
2025-03-25 16:17:40 +0200

    upgrade playground to test it

--- src/test/java/playground.java
+++ src/test/java/playground.java
@@ -1,15 +1,49 @@
-// import java.util.ArrayList;
 
-// import com.yetnt.*;
-// import com.yetnt.Token.TBooleanVar;
-// import com.yetnt.Token.TIntVar;
-// import com.yetnt.Token.TStringVar;;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-// public class playground {
+import com.yetnt.tokenizer.ContextDispatcher;;
 
-// public static void main(String[] args) {
+public class playground {
 
-// System.out.println("-".repeat(0));
+    public static void main(String[] args) {
+        ArrayList<String> testCases = new ArrayList<>(Arrays.asList(
+                "()",
+                "arr[2]",
+                "arr[2 - 4]",
+                "func(arr[2]) - arr[200]",
+                "[]",
+                "",
+                "a",
+                "1 + a",
+                "a + func(b)",
+                "1 + a - (a / b)",
+                ")",
+                "(",
+                "func()",
+                "func(func(d))",
+                "(1 - c) + n",
+                "func(a + b)",
+                "func(a + (b - a))",
+                "func(func(a - b))",
+                "func(func(a) | b)",
+                "func((1 > 1) | func(10))",
+                "(1 > 1) | func(10)",
+                "func(a) + b",
+                "func(func(b)) - a",
+                "func(a + func(c)) + v",
+                "func(2) > func(4)",
+                "func(func(b - g)) - func(c)",
+                "func(c) - func(c) -func(c) - func(func(c))",
+                "func(l) | (f > b)",
+                "arr[func(c) - func(c) -func(c) - func(func(c))]",
+                "func(c) - func(c) -func(c) - arr[func(c)]"));
 
-// }
-// }
+        testCases.forEach(c -> {
+            System.out.println();
+            ContextDispatcher d = new ContextDispatcher(c);
+            System.out.println(d.bits + " " + c + "\t\t| " + d.printCase() + " | " + d.toBitString());
+        });
+
+    }
+}
