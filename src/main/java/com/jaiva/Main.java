6f69c3bac0544c4a0c3066fc527d034303bdfbcb
Lehlogonolo Poole <hlonipoole692@gmail.com>
2025-04-12 12:19:47 +0200

    CLI commands and ASCII art

--- src/main/java/com/jaiva/Main.java
+++ src/main/java/com/jaiva/Main.java
@@ -3,6 +3,7 @@ package com.jaiva;
 import java.io.File; // Import the File class
 import java.io.FileNotFoundException; // Import this class to handle errors
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 import com.jaiva.interpreter.Context;
@@ -15,39 +16,74 @@ import com.jaiva.utils.BlockChain;
 import com.jaiva.utils.Find;
 
 public class Main {
+    public static String ASCII = """
+                                                                        ,---,
+                     ,---._                                           ,`--.' |
+                   .-- -.' \\                                          |   :  :
+                   |    |   :              ,--,                       '   '  ;
+                   :    ;   |            ,--.'|                       |   |  |
+                   :        |            |  |,      .---.             '   :  ;
+                   |    :   :  ,--.--.   `--'_    /.  ./|   ,--.--.   |   |  '
+                   :          /       \\  ,' ,'| .-' . ' |  /       \\  '   :  |
+                   |    ;   |.--.  .-. | '  | |/___/ \\: | .--.  .-. | ;   |  ;
+               ___ l          \\__\\/ : . . |  | :.   \\  ' .  \\__\\/ : . . `---'. |
+             /    /\\    J   : ," .--.; | '  : |_\\   \\   '  ," .--.; |  `--..`;
+            /  ../  `..-    ,/  /  ,.  | |  | '.'\\   \\    /  /  ,.  | .--,_
+            \\    \\         ;;  :   .'   \\;  :    ;\\   \\ |;  :   .'   \\|    |`.
+             \\    \\      ,' |  ,     .-./|  ,   /  '---" |  ,     .-./`-- -`, ;
+              "---....--'    `--`---'     ---`-'          `--`---'      '---`"
+
+                        """;
+
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
+        ArrayList<String> replArgs = new ArrayList<>(
+                Arrays.asList("--print-tokens", "-p", "--help", "-h", "--version", "-v"));
+        ArrayList<String> tokenArgs = new ArrayList<>(Arrays.asList("-s", "-j", "--string", "-json"));
         if (args.length == 0) {
-            System.out.println("Args length is 0, so ima lowkey just start a REPL");
             new REPL(0);
             return;
-        } else if (args[0].equals("-m")) {
-            if (args.length == 1) {
-                System.out.println("You need to specify a REPL mode.");
-                return;
-            }
-            switch (args[1]) {
-                case "standard", "s":
-                    System.out.println("Standard REPL mode.");
-                    new REPL(0);
-                    break;
-                case "print-tokens", "p":
-                    System.out.println("Print token REPL mode.");
+        } else if (replArgs.contains(args[0])) {
+            switch (args[0]) {
+                case "--print-tokens", "-p":
+                    // System.out.println("Print token REPL mode.");
                     new REPL(1);
                     break;
-                case "arithmetic-logic", "am":
-                    System.out.println("Arithmetic logic REPL mode.");
-                    new REPL(2);
+                case "--help", "-h":
+                    System.out.println();
+                    System.out.println("Usage: jaiva [options]");
+                    System.out.println("Options:");
+                    System.out.println("(no flag): Starts a REPL.");
+                    System.out.println("--print-tokens, -p: Print tokens REPL mode.");
+                    System.out.println("--help, -h: Show this help message.");
+                    System.out.println("--version, -v: Show the version of Jaiva.");
+                    System.out.println();
+                    System.out.println("Usage: jaiva <file.jiv> [options]");
+                    System.out.println("Options:");
+                    System.out.println("(no flag): Run the file.");
+                    System.out.println("--json, -j: Print tokens in JSON format.");
+                    System.out.println("--string, -s: Print tokens in string format.");
+                    System.out.println();
+                    break;
+                case "--version", "-v":
+                    System.out.println(ASCII);
+                    System.out.println("Jaiva! 1.0.0");
+                    System.out.println(
+                            "Jaiva is a programming language that is designed to be easy to use and understand. (I'm speaking out my ass)");
                     break;
                 default:
                     System.out.println("Invalid REPL mode.");
                     break;
             }
             return;
+        } else if (!args[0].contains(".")) {
+            // this is to catch the case where the user does not provide a file name
+            System.out.println("Provide a file name or a cmd flag kau.");
+            return;
         }
         // here, args[0] is the file name that must end in either .jaiva, .jiv or .jva
         if (!args[0].endsWith(".jaiva") && !args[0].endsWith(".jiv") && !args[0].endsWith(".jva")) {
-            System.out.println("> File must end in .jaiva, .jiv or .jva");
+            System.out.println("I can't read whatever that lang is bro (i accept only .jaiva, .jiv or .jva)");
             return;
         }
         File myObj = new File(args[0]);
@@ -86,28 +122,31 @@ public class Main {
             // System.out.println(tokens.size());
             myReader.close();
 
-            if (args[1].equals("-t") || args[1].equals("--tokens")) {
+            if ((args.length > 1) && tokenArgs.contains(args[1])) {
                 // here, args[1] is the tokens mode
                 // return tokens mode.
-                if (args.length == 2) {
-                    for (Token<?> t : tokens) {
-                        System.out.println(t.toString());
-                        // System.out.println(t.getValue().getContents(0));
-                    }
-                    return;
-                }
-                if (args[2].equals("json")) {
-                    System.out.println("Json mode.");
-                    System.out.println("[");
-                    for (int i = 0; i < tokens.size(); i++) {
-                        Token<?> token = tokens.get(i);
-                        System.out.print(token.getValue().toJson());
-                        if (i != tokens.size() - 1) {
-                            System.out.print(",\n");
+                switch (args[1]) {
+                    case "-s", "--string":
+                        for (Token<?> t : tokens) {
+                            System.out.println(t.toString());
+                            // System.out.println(t.getValue().getContents(0));
                         }
-                    }
-                    System.out.println("]");
-                    return;
+                        return;
+                    case "-j", "--json":
+                        System.out.println("Json mode.");
+                        System.out.println("[");
+                        for (int i = 0; i < tokens.size(); i++) {
+                            Token<?> token = tokens.get(i);
+                            System.out.print(token.getValue().toJson());
+                            if (i != tokens.size() - 1) {
+                                System.out.print(",\n");
+                            }
+                        }
+                        System.out.println("]");
+                        return;
+                    default:
+                        System.out.println("Invalid token mode.");
+                        return;
                 }
             }
             Interpreter.interpret(tokens, Context.GLOBAL, new Globals().vfs);
